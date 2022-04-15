68909e93147827394aedad5433cbe519ccdd011b
lopof <thomas.baumann90@gmx.ch>
2022-04-15 09:57:41 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sample-app app is running!');
+    expect(compiled.querySelector('.h1')?.textContent).toContain('Beat it');
   });
 });
